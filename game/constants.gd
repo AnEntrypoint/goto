bfb5c04378f6fe26bab2dafeaf52238b5b1c68f0
lanmower <almagestfraternite@gmail.com>
2026-01-03 16:26:28 +0200

    Delete game directory

--- game/constants.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-const SERVER_HOST = "127.0.0.1"
-const SERVER_PORT = 3008
--- game/main.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-func _ready() -> void:
-	var client = load("res://multiplayer/client.gd").new()
-	add_child(client)
